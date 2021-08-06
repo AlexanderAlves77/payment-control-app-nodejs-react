94f981c221cecd89762344342224d702b3c5eb4a
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-08-06 10:04:05 -0300

    Implementando a autenticação no backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function (server) {
-  // Definir URL base para todas as rotas
-  const router = express.Router()
-  server.use('/api', router)
+  // Rotas Protegidas por Token JWT
+  const protectedApi = express.Router()
+  server.use('./api', protectedApi)
 
-  // Rotas de Ciclo de Pagamento
-  const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
+  protectedApi.use(auth)
+
+  const billingCycle = require('../api/billingCycle/billingCycleService')
+  billingCycle.register(protectedApi, '/billingCycles')
+
+  // Rotas Abertas
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+
+  const AuthService = require('../api/user/AuthService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
